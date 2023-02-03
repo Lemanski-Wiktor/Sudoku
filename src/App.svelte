0780cf6e83d120dd0f096afdbd60d72cd0de8006
Wikor Lemanski <wiktorlem01@gmail.com>
2023-02-03 13:40:24 +0100

    Init project

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { Router, Link, Route } from "svelte-routing";
+	import LoadFile from "./components/LoadFile.svelte";
+	import Game from "./components/Game.svelte";
+
+	export let url = "";
+
+</script>
+	
+<main>
+	<Router url="{url}">
+
+		<Route path='' component='{LoadFile}'/>
+		<Route path='game' component='{Game}'/>
+
+	</Router>
+</main>
+
+<style>
+	main {
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
---
+++ src/components/LoadFile.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {Link} from "svelte-routing";
+
+</script>
+
+<div id="container">
+    <form action="">
+        <h2>Load data for Sudoku:</h2>
+        <input type="file" name="data" id="data">
+        <button><Link to='game'>Load</Link></button>
+    </form>
+</div>
+
+<style>
+    form{
+        width: 55vw;
+        height: 40vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
+        background-color: azure;
+        border: 1px solid black;
+    }
+    #data{
+        width: 250px;
+    }
+</style>
